01d99edef84373de4419d102af49ccdce221d444
masterthepixel <masterthepixel@gmail.com>
2023-03-05 12:53:37 -0500

    ENV gitignore

--- src/compositions/ScoresScreen.tsx
+++ src/compositions/ScoresScreen.tsx
@@ -107,8 +107,8 @@ function ScoresScreen({
         <div className="max-w-2xl mx-auto space-y-16 divide-y divide-gray-100 lg:mx-0 lg:max-w-none">
           <div className="grid grid-cols-1 gap-y-10 gap-x-8 lg:grid-cols-3">
             <div>
-              <h2 className="text-2xl font-black tracking-tight text-gray-900"></h2>
-              <p className="mt-4 font-extrabold leading-7 text-yellow-400">
+              <h2 className="font-black tracking-tight text-gray-900"></h2>
+              <p className="mt-4 text-2xl font-extrabold leading-7 text-yellow-400">
                GLENN DALE TIRE, AUTO & ROADSIDE ASSISTANCE
               </p>
             </div>
