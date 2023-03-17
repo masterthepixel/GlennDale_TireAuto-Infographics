8303a71b438b611bf79ad2e956fde342cb4f4062
maruffahmed <marufah76@gmail.com>
2023-03-17 15:36:13 +0600

    Fixed local render issue

--- render-all.sh
+++
@@ -1,6 +0,0 @@
-compositions=$(npx remotion compositions src/index.tsx -q)
-
-for composition in $compositions
-do
-  npx remotion render --range 84 300 src/index.tsx $composition out/$composition.mp4 --concurrency=1
-done
