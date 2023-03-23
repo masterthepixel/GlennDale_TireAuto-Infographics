4e0fac7cf126610472bcc2ff811a27031db6c7b6
masterthepixel <masterthepixel@gmail.com>
2023-03-23 08:47:34 -0400

    Update remotion.config.ts
    Improve render performance

--- remotion.config.ts
+++ remotion.config.ts
@@ -1,3 +1,11 @@
 import { Config } from "remotion";
 import { enableTailwind } from "./src/enable-tailwind";
+
+// Enable Tailwind CSS
 Config.Bundling.overrideWebpackConfig(enableTailwind);
+
+// Configure Remotion to export videos to WebP format with the following options
+Config.Output.setImageFormat("webp");
+Config.Output.setPixelFormat("yuv420p");
+Config.Output.setCodec("libwebp");
+Config.Output.setQuality("75");
